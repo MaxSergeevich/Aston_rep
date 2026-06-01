f95f31a33b48fa32ef2c18dd67d519ba4545206a
maxim <maximsergeevich@bk.ru>
2026-06-01 16:36:36 +0700

    lesson_8

---
+++ src/src/main/java/lesson_8/SeleniumDriver.java
@@ -0,0 +1,24 @@
+package lesson_8;
+
+import lombok.Setter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Objects;
+
+public class SeleniumDriver {
+
+    public static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    @Setter
+    private static ChromeOptions options;
+
+    public static WebDriver getInstance() {
+        if (Objects.isNull(driver.get())) {
+            driver.set(new ChromeDriver(options));
+        }
+        return driver.get();
+    }
+
+}
